ac178af42266088171de1b67ab2c142565869fb5
JoolsBlinnikova <ulyalichka83@gmail.com>
2019-07-25 17:51:14 +0300

    some fixed

--- src/main/java/openway/controller/MobileController.java
+++ src/main/java/openway/controller/MobileController.java
@@ -11,9 +11,6 @@ import java.util.logging.Logger;
 @RestController
 public class MobileController {
 
-
-    }
-
     private final static Logger logger = Logger.getLogger(MainController.class.getName());
 
     private final ClientService clientService;

80f0f850d2b3eb19077e001dfee0eabf31864355
JoolsBlinnikova <ulyalichka83@gmail.com>
2019-08-05 10:37:16 +0300

    added lessor page,modified login and reg pages

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,10 +8,12 @@ import Reg from './components/Registration';
 
 
 import InfoLesser from './components/Info';
-import RegMotoroller from './components/RegMotoroller';
+import RegScooter from './components/RegScooter';
 import Lesser from "./components/Lesser";
 import Logout from "./components/Logout";
-
+import DepositMoney from "./components/DepositMoney";
+import PaymentInfo from "./components/PaymentInfo";
+import ScooterInfo from "./components/ScooterInfo";
 
 ReactDOM.render(
     <div>
@@ -30,13 +32,22 @@ ReactDOM.render(
                        render={() => <InfoLesser/>}
                 />
                 <Route exact path="/regmoto"
-                       render={() => <RegMotoroller/>}
+                       render={() => <RegScooter/>}
                 />}
                 <Route exact path="/lesser"
                        render={() => <Lesser/>}
                 />
                 <Route exact path="/logout"
                        render={() => <Logout/>}
+                />
+                <Route exact path="/deposit_money"
+                       render={() => <DepositMoney/>}
+                />
+                <Route exact path="/payment_info"
+                       render={() => <PaymentInfo/>}
+                />
+                <Route exact path="/scooter_info"
+                       render={() => <ScooterInfo/>}
                 />
                 <Route component={NotFound}/>
             </Switch>

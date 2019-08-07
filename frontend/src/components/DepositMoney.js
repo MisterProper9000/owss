8ae894ca536b10ef5d16143b44819386431356de
JoolsBlinnikova <ulyalichka83@gmail.com>
2019-08-07 11:39:48 +0300

    fixed deposit

--- frontend/src/components/DepositMoney.js
+++ frontend/src/components/DepositMoney.js
@@ -1,14 +1,17 @@
 import React, {Component} from 'react';
 import '../css/DepositMoney.css';
+import '../css/Login.css';
 import NavbarComp from "./NavbarComp";
 
 import {AndreyLocalIpOW, JuliaLocalIpOW} from "./ipConfigs";
+import Cookies from "js-cookie";
 
 class DepositMoney extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
+            id: Cookies.get('token'),
             card_number: '',
             security_code: '',
             name_on_card: '',
@@ -26,7 +29,12 @@ class DepositMoney extends Component {
 
     handleSubmit(event) {
         event.preventDefault();
-        const {card_number, security_code, name_on_card, expiration, depositMoney} = this.state;
+        const {card_number, security_code, expiration, depositMoney, id} = this.state;
+
+        if (this.state.card_number === "" || this.state.security_code === "" || this.state.expiration === "" || this.state.depositMoney === "") {
+            this.setState({errorMsg: 'All fields must be filled'});
+            return;
+        }
 
         fetch(JuliaLocalIpOW + '/depositmoney', {
             //fetch('http://10.101.177.21:9091/depositmoney', {
@@ -36,25 +44,26 @@ class DepositMoney extends Component {
                     card_number,
                     security_code,
                     expiration,
-                    depositMoney
+                    depositMoney,
+                    id
                 }
             )
         })
-            .then(resp =>{
-                if(resp){
-                    alert("You have successfully replenished your wallet in the amount of "+depositMoney)
-                }
-                else {
+
+            .then(resp => {
+                if (resp) {
+                    alert("You have successfully replenished your wallet in the amount of " + depositMoney)
+                } else {
                     this.setState({errorMsg: 'Replenishment error'});
                 }
-            });
-    }
-
-    componentDidMount() {
+            })
     }
 
     goBack() {
         window.location = "/lesser";
+    }
+
+    componentDidMount() {
     }
 
     render() {
@@ -63,16 +72,17 @@ class DepositMoney extends Component {
         return (
             <div>
                 <NavbarComp/>
-                <button className="buttonBack" onClick={this.goBack}> Back</button>
+
+
                 <form className="formDeposit" onSubmit={this.handleSubmit}>
                     <h1 className="title">Deposit Money</h1>
-                    <input className="input" type="text" placeholder="card_number" name="Card Number"
+                    <input className="input" type="text" placeholder="card number" name="card_number"
                            value={card_number}
                            onChange={this.handleChange}/><br/>
-                    <input className="input" type="text" placeholder="security_code" name="Security Code"
+                    <input className="input" type="text" placeholder="security code" name="security_code"
                            value={security_code}
                            onChange={this.handleChange}/><br/>
-                    <input className="input" type="text" placeholder="expiration" name="Expiration"
+                    <input className="input" type="text" placeholder="expiration" name="expiration"
                            value={expiration}
                            onChange={this.handleChange}/><br/>
                     <br/>
@@ -80,7 +90,7 @@ class DepositMoney extends Component {
                     <input className="input" type="text" placeholder="Cost" name="depositMoney"
                            value={depositMoney}
                            onChange={this.handleChange}/><br/>
-                    <input type="submit" name="buttonLogin" className="buttonSubmit"
+                    <input type="submit" name="buttonDeposit" className="buttonSubmitDeposit"
                            value="Submit"/>
                 </form>
             </div>
